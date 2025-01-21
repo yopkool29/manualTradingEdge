3112b79b11bbf07c2dda79f68aae3a5918f15247
Patrice Lindivat <plindivat@free.fr>
2025-01-21 11:58:20 +0100

    upgrade default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,9 +16,16 @@
                         <NuxtLink to="/settings" class="text-gray-700 hover:text-gray-900">{{ $t('menu.settings') }}</NuxtLink>
                     </li>
                 </ul>
-                <button @click="toggleLanguage" class="flex items-center px-3 py-2 rounded-lg text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors">
-                    <span class="font-medium">{{ $t('language.switch') }}</span>
-                </button>
+                <div class="flex items-center space-x-4">
+                    <span>
+                        <a href="https://github.com/yopkool29/manualTradingEdge" target="_blank" rel="noopener noreferrer" class="flex items-center text-gray-700 hover:text-gray-900">
+                            <UIcon name="i-simple-icons-github" class="w-6 h-6" />
+                        </a>
+                    </span>
+                    <button @click="toggleLanguage" class="flex items-center px-3 py-2 rounded-lg text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors">
+                        <span class="font-medium">{{ $t('language.switch') }}</span>
+                    </button>
+                </div>
             </nav>
 
         </header>
@@ -26,6 +33,19 @@
         <main class="container mx-auto px-4 py-8">
             <slot />
         </main>
+        <!-- Pied de page -->
+        <footer class="fixed bottom-0 left-0 w-full mx-auto px-8 py-2 bg-white shadow flex items-center">
+            <p class="text-left text-gray-600">&copy; {{ new Date().getFullYear() }} - Yopkool</p>
+            <div class="flex ml-4 gap-x-2 items-center">
+                <span class="text-gray-700">
+                    github =>
+                </span>
+                <a href="https://github.com/yopkool29/manualTradingEdge" target="_blank" rel="noopener noreferrer" class="flex items-center text-gray-700 hover:text-gray-900">
+                    <UIcon name="i-simple-icons-github" class="w-6 h-6" />
+                </a>
+            </div>
+        </footer>
+
     </div>
 </template>
 
