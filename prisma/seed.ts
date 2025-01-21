b5a16df7d3e7a68a58faa7a75a2779e6022dc5b8
Patrice Lindivat <plindivat@free.fr>
2025-01-21 11:57:40 +0100

    update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,22 +3,22 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  // Create default settings if they don't exist
-  const settings = await prisma.settings.findFirst()
-  if (!settings) {
-    await prisma.settings.create({
-      data: {
-        pointValues: [2, 5, 10, 15, 20, -2, -5, -10, -15, -20]
-      }
-    })
-  }
+    // Create default settings if they don't exist
+    const settings = await prisma.settings.findFirst()
+    if (!settings) {
+        await prisma.settings.create({
+            data: {
+                pointValues: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -20],
+            },
+        })
+    }
 }
 
 main()
-  .catch((e) => {
-    console.error(e)
-    process.exit(1)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+    .catch((e) => {
+        console.error(e)
+        process.exit(1)
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
